bb18362c783563f3b0a831cdec9aab5648029795
Eshaan0110 <eshaan.adyanthay@gmail.com>
2025-02-09 16:14:24 +0530

    image enhancement added

--- DATA_LOAD.py
+++ DATA_LOAD.py
@@ -2,6 +2,8 @@ from PIL import Image
 from torch.utils.data import Dataset
 from XML_PARSER import xml_parse
 import torch
+import numpy as np
+import cv2
 
 class data_loading(Dataset):
     def __init__(self, image_dir, annotations_dir, transforms=None):
@@ -22,6 +24,25 @@ class data_loading(Dataset):
         l_n = {"No-Gate" : 0, "Gate" : 1}
         return l_n[label]
     
+    def filter(self, img):
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        b, g, r = cv2.split(img_rgb)
+        b = cv2.normalize(b, None, 0, 255, cv2.NORM_MINMAX)
+        g = cv2.normalize(g, None, 0, 255, cv2.NORM_MINMAX)
+        r = cv2.normalize(r, None, 0, 255, cv2.NORM_MINMAX)
+        img_corrected = cv2.merge((b, g, r))
+    
+    
+        lab = cv2.cvtColor(img_corrected, cv2.COLOR_RGB2Lab)
+        l_channel, a_channel, b_channel = cv2.split(lab)
+    
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(10, 50))
+        cl = clahe.apply(l_channel)
+    
+        lab_corrected = cv2.merge((cl, a_channel, b_channel))
+        img_enhanced = cv2.cvtColor(lab_corrected, cv2.COLOR_Lab2BGR)
+        return img_enhanced
+    
     def __getitem__(self, idx):
         annotation = self.annotations[idx]
         filename = annotation['filename']
@@ -30,6 +51,9 @@ class data_loading(Dataset):
         label = annotation['label']
         img = Image.open(f"{self.image_dir}/{filename}")
         img = img.resize((224, 224))
+        img = np.array(img)
+        img = filter(img)
+
 
         label = torch.tensor(self.label_to_int(label), dtype=torch.float32)
         keypoints = torch.tensor(keypoints, dtype=torch.float32)
